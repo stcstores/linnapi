8b7688883b831a41bf23ba26480ed279e46bdb61
Luke Shiner <luke@lukeshiner.com>
2022-06-16 11:06:58 +0100

    Add orders to __init__.py

--- linnapi/__init__.py
+++ linnapi/__init__.py
@@ -1,6 +1,12 @@
 """linnapi - Linnworks API integration."""
 
-from . import exceptions, inventory
+from . import exceptions, inventory, orders
 from .session import LinnworksAPISession, linnworks_api_session
 
-__all__ = ["exceptions", "inventory", "LinnworksAPISession", "linnworks_api_session"]
+__all__ = [
+    "exceptions",
+    "inventory",
+    "orders",
+    "LinnworksAPISession",
+    "linnworks_api_session",
+]
